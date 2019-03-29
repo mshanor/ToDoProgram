dc9166203ce3415d0149ac542e58cfa57fee1ffa
Matt Shanor <46903961+mshanor@users.noreply.github.com>
2019-03-28 21:42:43 -0500

    Add files via upload

---
+++ Tasks/Tasks/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+/*Program:To-Do List
+ * Author: Matthew Shanor 
+ * Date: 3-28-2019
+ * v:1
+ * This program is meant as a familirization with c#. This is a simple program to take in tasks via
+ * command line and add them to a list. Each task has a date, priority, and status. User can Add 
+ * tasks, date, status, priority. Once a task is completed it is removed from the List.
+ */
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //local variables
+            char choice;
+            bool entry = false;
+            int a;
+
+            //Introduction message to user.
+            Console.Write("Welcome to the Task program a console based program. A simple task tracking program.\n");
+            Console.Write("This app allows you to add a task to be completed. Set a time limit for each task.\n");
+            Console.Write("As well as showing priority and completion status of each task.\n");
+            Console.Write("Would you like to continue into the program Yes (Y) or No (N)?");
+            
+            //check user input to continue or not.
+            do
+            {
+                //read in choice from user
+                choice = char.Parse(Console.ReadLine());
+
+                if (choice == 'Y')
+                {
+                    entry = true;
+                }
+                else if (choice == 'N')
+                {
+                    Console.Write("Goodbye.");
+                    System.Environment.Exit(0);
+                }
+                else
+                    Console.Write("Invalid entry please enter (Y) or (N)");
+
+            } while (entry == false);
+
+            //provide user with options for how to use program
+            Console.Write("Enter (1) to add a task. (2) to show all taks. (3) to complete a task. (4) Exit: ");
+
+            //Create newList for user to start using program
+            ToDo newList = new ToDo();
+
+            //loop until user wants to exit
+            while (true)
+            {
+                
+                //store users input
+                a = int.Parse(Console.ReadLine());
+                
+                //check which case user inputted and call that procedure in ToDo
+                switch (a)
+                {
+                    case (1):
+                        newList.AddTask();
+                        Console.Write("Enter another selection: ");
+                        break;
+
+                    case (2):
+                        newList.ShowList();
+                        Console.Write("Enter another selection: ");
+                        break;
+                    case (3):
+                        newList.Complete();
+                        Console.Write("Enter another selection: ");
+                        break;
+
+                    case (4):
+                        Console.WriteLine("GoodBye!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid entry please input valid option. ");
+                        break;
+
+                }
+                //condition to break while loop
+                if (a == 4)
+                {
+                    break;
+                }
+            }
+            //pause at end of program
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tasks/Tasks/ToDo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Tasks
+{
+    class ToDo
+    {
+        //Class Variables
+        private string task { get; set;}
+        private DateTime date{get;set;}
+        private string priority { get; set; }
+        //Input variables from user
+        public string a;
+        public DateTime b;
+        public string c;
+
+        //default constructor
+        public ToDo()
+        {
+            
+        }
+
+        //constructor
+        public ToDo(string _name, DateTime _date, string _priority)
+        {
+            task = _name; //task variable initialized to user input
+            date = _date; //date variable intialized to user input
+            priority = _priority; //priority variable initialized to user input
+        }
+        //setup new list for todo tasks
+        public List<ToDo> myList = new List<ToDo>();
+
+        //User has chosen to add tasks to their list.
+        public void AddTask()
+        {
+            Console.Write("Enter task: ");
+            a = Console.ReadLine();
+
+            Console.Write("Please enter due date in this format yyyy-mm-dd: ");
+            b = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("What is the Priority High, Mid, Low: ");
+            c = Console.ReadLine();
+
+            //create ToDo instance tasks to add to list.
+            ToDo tasks = new ToDo( a, b, c);
+
+            //add to list tracking tasks
+            myList.Add(tasks);    
+
+        }
+
+
+        //method to display all current tasks and their priorities
+       public void ShowList()
+        {
+            //Task ID variable
+            int i = 0;
+            //loop through list 
+            foreach (ToDo element in myList)
+            {
+                //Print to conosle task ID/Task/Date/Priority
+                Console.WriteLine($"{i}" + " - " + element.task + ", " + element.date.ToShortDateString() + ", " + element.priority);
+                //increment ID
+                i++;
+            }
+        }
+
+        //method to remove completed tasks from each list
+        public void Complete()
+        {
+            //Check if list is empty signal user if so. Otherwise find completed task and remove from list.
+            if (myList.Count != 0)
+            {
+                int z;
+                Console.Write("Which task is complete?: ");
+                z = int.Parse(Console.ReadLine());
+                myList.RemoveAt(z);
+            }
+            else
+                Console.WriteLine("No current tasks available.");
+        }
+    }
+}
